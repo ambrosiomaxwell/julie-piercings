df327ca3975fcef05ceea09ddfb6bb05eaa3c662
Madeline Kerchner <madelinekerchner@Madelines-MacBook-Pro.local>
2021-03-13 18:47:43 -0500

    second stuctural commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
+const { get } = require('http');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,21 +17,37 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+require('./routes/api-routes.js')(app);
+
 //homepage
 app.get('/',(res,req)=>{
 
     res.render('index.html')
-})
+});
 
 
 //scheduling page
 app.get('/scheduling',(res,req) =>{
 
-})
+});
 
-//custom scheduled page
+//custom scheduled page per appointment
 app.get('/scheduling/:name', (res,req) =>{
 
-})
-require('./routes/api-routes.js')(app);
+});
+
+
+//for creating new appointment
+app.post('/scheduling/new-scheduling',(res,req)=>{
+
+});
+
+//confirming appointment
+app.post('schedule/admin/confirm/:id', (res,req, auth)=> {
+
+});
+
+
+
+
 app.listen(PORT)
