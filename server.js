1e52c9b8ba5e6392fd5080b5fd2bd70c1f718ec5
Madeline Kerchner <madelinekerchner@Madelines-MacBook-Pro.local>
2021-03-13 18:29:39 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//homepage
+app.get('/',(res,req)=>{
+
+    res.render('index.html')
+})
+
+
+//scheduling page
+app.get('/scheduling',(res,req) =>{
+
+})
+
+//custom scheduled page
+app.get('/scheduling/:name', (res,req) =>{
+
+})
+require('./routes/api-routes.js')(app);
+app.listen(PORT)
